09743e2117322db7fce34dfe5a288ff161c4b300
Levi Murray <levi@cloudpractice.ca>
2019-07-01 15:41:31 -0700

    Fixed api path + Production build

--- client/src/lib/api/base.api.ts
+++ client/src/lib/api/base.api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 class BaseApi {
-	private API_URI: string = "https://typetris/api";
+	private API_URI: string = "https://typetris.com/api";
 
 	protected get(endpoint: string): Promise<any> {
 		const path = `${this.API_URI}${endpoint}`;
